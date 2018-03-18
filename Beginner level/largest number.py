ac081996ddd156c4d4bb973d79319afaf6e017cb
CATHRIN-h <37336256+CATHRIN-h@users.noreply.github.com>
2018-03-18 15:49:24 +0530

    Create largest number.py

---
+++ Beginner level/largest number.py
@@ -0,0 +1,9 @@
+a1=int(input('enter the num1:'))
+a2=int(input('enter the num2:'))
+a3=int(input('enter the num3:'))
+if((a1>a2)or(a1>a3)):
+ print(a1 is larger)
+elif((a2>a1)or(a2>a3)):
+ print(a2 is larger)
+ else:
+ print(a3 is larger)
