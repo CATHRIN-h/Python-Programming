a679d7c75d8f86b3c49325793ba78eb31c8ea9ad
CATHRIN-h <37336256+CATHRIN-h@users.noreply.github.com>
2018-03-20 14:27:24 +0530

    Update even.py

--- Beginner level/even.py
+++ Beginner level/even.py
@@ -1,7 +1,6 @@
-h=int(input('enter the number'))
-g=int(input('enter the number'))
-for i in range(h,g+1):
-    	if ((i%2) != 0):
-    	    print("i")
-    	else:
-    	    print("")
+while(true):
+l=int(input('enter the initial number'))
+j=int(input('enter the final number'))
+for i in range(l,j):
+    if(i%2==0):
+        print('even number',i)
