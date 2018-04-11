e0c27adc18a17dd517dd08d22354766f50c42af2
CATHRIN-h <37336256+CATHRIN-h@users.noreply.github.com>
2018-04-11 18:38:00 +0530

    Create hour.py

---
+++ Beginner level/hour.py
@@ -0,0 +1,11 @@
+a1=int(input("enter the time 1:")
+b1=int(input("enter the minute 1:")
+a2=int(input("enter the time 2":))
+b2=int(input("enter the minute 2":))
+if(a1>0 and b1>0):
+a3=a1-a2
+b3=b1-b2
+print(a3,b3)
+else:
+print(-a3,-b3)
+
